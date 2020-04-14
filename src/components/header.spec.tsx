f879b8532cba484b00b37b224ee28aa7e15da7af
Jack McGregor <jmcgregor@coveo.com>
2020-04-14 19:06:37 -0400

    added base stylesheets

--- src/components/header.spec.tsx
+++ src/components/header.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import Header from "../header"
+import Header from "./header"
 
 describe("Header", () => {
     it("renders correctly", () => {
